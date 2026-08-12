2d835f82ff7344aab108dd98a814ea2650163dca
Muhammad Hassan Raza <hassan.mhr03@gmail.com>
2026-08-12 15:24:30 +0500

    fix: create add schema validation and authentication fields to user schema

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true, trim: true, minlength: [3, 'Name must be at least 3 characters long'], maxlength: [30, 'Name must be at most 30 characters long'] },
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
     // storing as bcrypt hash
     password: { type: String, required: true },
     // hashed refresh token... null when logged out
